180dc37d82dd0cf2aa91f052c1de88ee3e7a52fb
Leon Markovich <leonmarkovich97@gmail.com>
2021-04-19 23:43:55 +0300

    added class poeple sum validator

---
+++ include/peopleSumFieldsValidator.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Field.h"
+template<typename T>
+class peopleSumFieldsValidator :Validator<T>{
+ 
+	peopleSumFieldsValidator(const std::string& errorMsg, )
+};
+peopleSumFieldsValidator()
