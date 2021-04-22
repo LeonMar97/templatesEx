5c986002772f742e832bae887e9c20cfb8efe5b5
ronehe <Ro.Heifetz@gmail.com>
2021-04-22 08:12:09 +0300

    new date algorithm, added sumvalidator

--- include/DateValidator.h
+++ include/DateValidator.h
@@ -20,20 +20,21 @@ DateValidator<T>::DateValidator()
 template<typename T>
 bool DateValidator<T>::checkValid() {
 	T value = *(this->m_ptrInfoV[0]);
-	using clock = std::chrono::system_clock;
-	const auto now = clock::to_time_t(clock::now());
-	auto calendarTime = std::tm{};
-	localtime_s(&calendarTime, &now);
-	if (value % 10000 < calendarTime.tm_year + 1900)
-		return false;
-	
-	if (((value % 1000000) / 10000) < calendarTime.tm_mon + 1) 
-		return false;
-	
-	if (((value % 1000000) / 10000) == calendarTime.tm_mon + 1&& 
-			(value / 1000000 < calendarTime.tm_mday))
-				return false;
-		
-	return true;
+    using clock = std::chrono::system_clock;
+    const auto now = clock::to_time_t(clock::now());
+    auto calendarTime = std::tm{};
+    localtime_s(&calendarTime, &now);
+    if (value % 10000 > calendarTime.tm_year + 1900)
+        return true;
+    if (value % 10000 < calendarTime.tm_year + 1900)
+        return false;
 
+    if (((value % 1000000) / 10000) < calendarTime.tm_mon + 1)
+        return false;
+    if (((value % 1000000) / 10000) > calendarTime.tm_mon + 1)
+        return true;
+
+    if (value / 1000000 < calendarTime.tm_mday)//if we on the same year and month 
+        return false;
+    return true;
 }
